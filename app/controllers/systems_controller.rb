c58e42a5fff492ab3599879dae1b084e6c5165eb
Alex Gusev <hello.from.code@gmail.com>
2013-12-07 00:17:54 +0200

    compact companies

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -7,7 +7,7 @@ class SystemsController < ApplicationController
       @collection[s.company_id] ||= []
       @collection[s.company_id] << s
     end
-    @companies.sort_by!(&:position).reverse!.uniq!
+    @companies.compact!.sort_by!(&:position).reverse!.uniq!
   end
 
   def home
@@ -18,7 +18,7 @@ class SystemsController < ApplicationController
       @collection[s.company_id] ||= []
       @collection[s.company_id] << s
     end
-    @companies.sort_by!(&:position).reverse!.uniq!
+    @companies.compact!.sort_by!(&:position).reverse!.uniq!
   end
 
   def show
